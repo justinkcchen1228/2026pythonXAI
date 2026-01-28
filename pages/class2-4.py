89f509a74cbfa1b45810310a4089c6594ae04a34
justinkcchen1228 <justinkcchen@gmail.com>
2026-01-28 16:52:34 +0800

    新增 Python 基礎學習筆記，包含比較運算子、邏輯運算子、運算子優先順序及 Streamlit 元件示範

--- pages/class2-4.py
+++ pages/class2-4.py
@@ -5,9 +5,6 @@ for i in range(1, number+1):
     st.write(str(i)*i)
 st.markdown("---")
 st.title("箭頭金字塔")
-number1=int(st.number_input("請輸入一個整數(1到9)",min_value=1,max_value=9,step=1,key="箭頭金字塔"))
-for i in range (1, number1+5):
-    if i<number1+1:
-        st.write(" "*int(((number1-i)/2))+"a"*i+" "*int(((number1-i)/2)))
-    else:
-        st.write(" "*int(((number1-1)/2))+"a"+" "*int(((number1-1)/2)))
+number1=int(st.number_input("請輸入箭頭的層數",min_value=1,max_value=9,step=1,key="箭頭金字塔"))
+for i in range(1,5):
+    st.write(" "*(number1-i)+"▲"* (2*i-1))
